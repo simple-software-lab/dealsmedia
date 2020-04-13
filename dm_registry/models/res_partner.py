f3f7dccd17b3e13016c8b58880229045ba21ceb7
Mark Meadows <mmeadows@dealsmedia.com>
2020-04-13 14:22:34 -0500

    brand approval test

--- dm_registry/models/res_partner.py
+++ dm_registry/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
-
+from bigoyster import Bigoyster
 
 class Partner(models.Model):
     _name = 'res.partner'
@@ -9,7 +9,19 @@ class Partner(models.Model):
 
     dashboard = fields.Char(string='Dashboard')
     location_handle = fields.Char(string='Handle')
+    registry_status = fields.Char(string='Status')
     active_campaigns = fields.Integer("Active Campaigns")
     follower_count = fields.Integer("Follower Count")
     credit_balance = fields.Integer("Credit Balance")
-    
+
+    def isa_brand(self):
+        return True
+
+    def approve_brand(self):
+        self.env.user.id
+        api_url = 'https://stageapi.bigoyster.com/'
+        user = 'test'
+        password = 'me'
+        token=''
+        client = Bigoyster(base_url=api_url, token=token, username=user, password=password)
+        
