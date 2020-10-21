398295d2887061425c118ba5a676a78c13cbd6ab
simple-software-lab <62183356+simple-software-lab@users.noreply.github.com>
2020-10-21 17:05:50 -0500

    Update res_partner.py

--- dm_registry/models/res_partner.py
+++ dm_registry/models/res_partner.py
@@ -2,6 +2,7 @@
 
 from odoo import api, fields, models
 from bigoyster import Bigoyster
+import requests
 
 class Partner(models.Model):
     _name = 'res.partner'
@@ -13,7 +14,31 @@ class Partner(models.Model):
     active_campaigns = fields.Integer("Active Campaigns")
     follower_count = fields.Integer("Follower Count")
     credit_balance = fields.Integer("Credit Balance")
-
+                                     
+    @api.model
+    def write(self, vals):
+        res = super(Partner).create(vals)
+        res.call_event_api('MFG018', {'odoo_id': res.id})
+        return res
+                                     
+    @api.model
+    def call_event_api(self, event_type, addl_info):
+        if os.environ.get('ODOO_STAGE') == 'staging':
+            url = 'https://stageapi.dealsmedia.com'
+            headers = {
+                'Authorization': "Token b4d38894b61f54b46de2239d9604446506772a69",
+            }
+        elif os.environ.get('ODOO_STAGE') == 'production':
+            url = 'https://api.dealsmedia.com/event/create'
+            headers = {
+                'Authorization': "Token f7a95eceaef62d1c5338dc20c54a0514c8c0070b",
+            }
+        params = {
+            'event_type': 'CO003',
+            'addl_info': addl_info
+        }
+        response = requests.post(url, json=params, headers=headers, timeout=5, verify=False)
+        
     def isa_brand(self):
         return True
 
